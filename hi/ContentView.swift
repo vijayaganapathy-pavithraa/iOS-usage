44b34c093b350ab2ae6c672992cf6171f3cf4ac1
vijayaganapathy-pavithraa <vijayaganapathy_pavithraa@s2023.ssts.edu.sg>
2023-09-18 17:03:52 +0800

    Initial Commit

---
+++ hi/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  hi
+//
+//  Created by Vijayaganapathy Pavithraa on 18/9/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("HEHE")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ hi/hiApp.swift
@@ -0,0 +1,17 @@
+//
+//  hiApp.swift
+//  hi
+//
+//  Created by Vijayaganapathy Pavithraa on 18/9/23.
+//
+
+import SwiftUI
+
+@main
+struct hiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
